8402cf2f66306e95d89a2ee02b0a788a2c33c114
arturohanyan1 <arturohanyan100@mail.ru>
2022-03-18 11:15:06 +0400

    add edit styles

--- routes/main.js
+++ routes/main.js
@@ -2,21 +2,20 @@ const router = require('express').Router();
 const { Listing } = require('../db/models');
 
 router.get('/', async (req, res) => {
-  // const allCards = await Listing.findAll({ order: [['createdAt', 'DESC']], raw: true });
-  // const notesOnPage = 8;
-  // const pageNum = 1;
-  // const start = (pageNum - 1) * notesOnPage;
-  // const end = start + notesOnPage;
-  // const currNote = allCards.slice(start, end);
+  const allCards = await Listing.findAll({ order: [['createdAt', 'DESC']], raw: true });
+  const notesOnPage = 8;
+  const pageNum = 1;
+  const start = (pageNum - 1) * notesOnPage;
+  const end = start + notesOnPage;
+  const currNote = allCards.slice(start, end);
 
-  // const arrLength = Math.ceil(allCards.length / notesOnPage);
-  // const btnArr = [];
-  // for (let i = 0; i < arrLength; i++) {
-  //   btnArr.push({ href: `/page/${i + 1}`, name: i + 1 });
-  // }
+  const arrLength = Math.ceil(allCards.length / notesOnPage);
+  const btnArr = [];
+  for (let i = 0; i < arrLength; i++) {
+    btnArr.push({ href: `/page/${i + 1}`, name: i + 1 });
+  }
 
-  // res.render('entries/main', { currNote, btnArr });
-  res.render('entries/checkout');
+  res.render('entries/main', { currNote, btnArr });
 });
 
 module.exports = router;
